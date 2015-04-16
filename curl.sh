0af9e62c0dfaec8bc11a9823ecbf87d574db77b9
Miantang <antisaber@gmail.com>
2015-04-16 09:36:27 +0800

    change

---
+++ curl.sh
@@ -0,0 +1,23 @@
+curl -i --request PUT  --data "{\"switch\":1}"  http://127.0.0.1/index.php/switches/1/switch
+curl -i --request POST  --data "{\"code\":0}"  http://127.0.0.1/index.php/feedback/led1
+
+curl -i --request POST  --data "{\"type\":\"switch\",\"value\":7}"  http://127.0.0.1/index.php/devices/1
+
+curl -i --request POST  --data "{\"type\":\"step\",\"switch\":7,\"controller\":1}"  http://127.0.0.1/index.php/devices/7
+
+
+curl -i --request PUT  --data "{\"switch\":1}"  http://192.168.1.1:8080/index.php/switches/1/switch
+curl -i --request POST  --data "{\"code\":0}"  http://192.168.1.1:8080/index.php/feedback/led1
+
+curl -i --request POST  --data "{\"type\":\"switch\",\"value\":7}"  http://192.168.1.1:8080/index.php/devices/1
+
+curl -i --request POST  --data "{\"type\":\"step\",\"switch\":7,\"controller\":1}"  http://192.168.1.1:8080/index.php/devices/7
+
+
+mosquitto_sub -v -t mqttdevice/1 -h 192.168.1.1
+
+mosquitto_pub -t mqttdevice/1 -h 192.168.1.1 -m "{\"type\":\"switch\",\"value\":0}"
+
+curl -i --request POST  --data "{\"type\":\"switch\",\"value\":7}"  http://192.168.1.1:8080/index.php/mqttdevices/1
+
+push test
