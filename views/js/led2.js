ca92c89337c1c3fa4bade19c8cdb63c043db657b
Miantang <antisaber@gmail.com>
2015-09-17 18:19:46 +0800

    fix color

--- views/js/led2.js
+++ views/js/led2.js
@@ -296,9 +296,15 @@ function tvViewModel() {
         {id : 1, value : 1, name : "1"},{id : 2, value : 2, name : "2"},{id : 3, value : 3, name : "3"},
         {id : 4, value : 4, name : "4"},{id : 5, value : 5, name : "5"},{id : 6, value : 6, name : "6"},
         {id : 7, value : 7, name : "7"},{id : 8, value : 8, name : "8"},{id : 9, value : 9, name : "9"},
-        {id : 10, value : 0, name : "0"},{id : 11, value : 11, name : "Mute"},
-        {id : 12, value : 12, name : "Left"},{id : 13, value : 13, name : "Right"},
-        {id : 14, value : 14, name : "Up"},{id : 15, value : 15, name : "Down"}
+        {id : 10, value : 0, name : "0"},{id : 11, value : 11, name : "M"},
+        {id : 12, value : 12, name : "L"},{id : 13, value : 13, name : "R"},
+        {id : 14, value : 14, name : "U"},{id : 15, value : 15, name : "D"}
+
+    ];
+    self.tvBtnGroup2 = [
+        {id : 10, value : 0, name : "0"},{id : 11, value : 11, name : "M"},
+        {id : 12, value : 12, name : "L"},{id : 13, value : 13, name : "R"},
+        {id : 14, value : 14, name : "U"},{id : 15, value : 15, name : "D"}
     ];
 
     self.loaddata = function () {
