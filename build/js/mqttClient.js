fa47b1f3c8ccdd1fb15e94312179d6f245674d5f
Miantang <antisaber@gmail.com>
2015-11-26 21:06:48 +0800

    blood and ip 116

--- build/js/mqttClient.js
+++ build/js/mqttClient.js
@@ -1,14 +1,15 @@
 define(['f7', 'knockout', 'ip'], function(f7, ko, IP){
     // MQTT
     var ip = IP.ip;
-    //var blood = ko.observable('0.0');
-    //ko.applyBindings({
-    //    blood: blood
-    //}, document.getElementById('blood'));
+    var blood = ko.observable('0.0');
+    ko.applyBindings({
+        blood: blood
+    }, document.getElementById('blood'));
 
     var client = mqtt.connect(ip().replace('http', 'ws')); // you add a ws:// url here
     client.subscribe("gas");
-    //client.subscribe("heart");
+    client.subscribe("heart");
+    //client.subscribe('wifi');
 
     client.on("message", function(topic, payload) {
         if(topic === 'gas') {
@@ -16,7 +17,7 @@ define(['f7', 'knockout', 'ip'], function(f7, ko, IP){
                 f7.alert('煤气可能泄漏，请注意关闭阀门！', '智能物联');
         } else if(topic === 'heart') {
 
-            //blood(payload);
+            blood(payload);
 
         } else {
             f7.addNotification({
